1d7ce22de281123f6fd3993e06bc6c2f970ad81d
suruchi810 <161755548+suruchi810@users.noreply.github.com>
2025-01-01 13:43:27 +0530

    Update page.jsx

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="xl:min-h-[260px] w-full rounded-[20px] bg-hero bg-cover bg-dark-1">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
 
-          <h2 className="glassmorphism h2-custom max-w-[273px] max-h-[100px] rounded pb-2 text-center text-base font-normal">
+          <h2 className="glassmorphism h2-custom max-w-[273px] max-h-[100px] m-2 rounded pb-2 text-center text-base font-normal">
             Upcoming Meeting at: 12:30 PM
           </h2>
           <div className="flex flex-col gap-2">
